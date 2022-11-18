c7f940a0f49a22eb988195f34ffaeed4c7b8b346
Your Name <you@example.com>
2022-11-18 12:16:35 -0300

    Variables en Java (Clase 2)

--- Java/src/Leccion1/HolaMundo/HolaMundo.java
+++ Java/src/Leccion1/HolaMundo/HolaMundo.java
@@ -6,5 +6,14 @@ package Leccion1.HolaMundo;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java");
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena="Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena="Sigamos creciendo en programacion";
+        System.out.println(miVariableCadena);
     }
 }
